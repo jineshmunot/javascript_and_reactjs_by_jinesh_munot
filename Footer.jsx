6ec634a1e47e963cf92944224074018685524917
jineshmunot <85447507+jineshmunot@users.noreply.github.com>
2021-06-06 20:36:32 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>copyright @ {currentYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascrip and React.js</h1>
+      <p>This was an amazing bootcamp taken by Shourya Sinha.
+      We covered HTML, Javascript, Reactjs from scratch.
+      The topic was very interesting and the teacher was also very good.
+      I hope that the next bootcamp would be longer and more interesting.</p>
+    </div>
+  );
+}
+
+export default Note;
